a442600f9ba222736ba9e55c2058adc05eaf7d41
Macohi <sinconsistencia@gmail.com>
2026-02-25 07:32:03 -0600

    game over

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,12 +1,20 @@
 package;
 
+import flixel.util.FlxTimer;
 import flixel.FlxState;
 
 class PlayState extends FlxState
 {
+	var time:Int = 60;
+
 	override public function create()
 	{
 		super.create();
+
+		FlxTimer.wait(time, function()
+		{
+			trace('Game over');
+		});
 	}
 
 	override public function update(elapsed:Float)
